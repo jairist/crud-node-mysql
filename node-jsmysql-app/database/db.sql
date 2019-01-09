1f1409794d955dd2ece13204f9cc3c65fcf269e0
jairist <jairist@gmail.com>
2019-01-09 18:57:05 -0400

    CRUP Listo

---
+++ node-jsmysql-app/database/db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE database_links;
+
+USE database_links;
+
+CREATE TABLE users(id INT(11) NOT NULL,username VARCHAR(16) NOT NULL,password VARCHAR(60) NOT NULL,fullname VARCHAR(100) NOT NULL)
+
+ALTER TABLE users
+ADD PRIMARY KEY (id);
+
+ALTER TABLE users MODIFY id INT(11) NOT NULL AUTO_INCREMENT = 1;
+
+DECRIBE users;
+
+-- links tables;
+ALTER TABLE links MODIFY created_ad INT(11) NOT NULL AUTO_INCREMENT = 1;
+
+CREATE TABLE links(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(150) NOT NULL, 
+    url VARCHAR(255) NOT NULL, 
+    description TEXT, 
+    user_id INT(11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp, 
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id))
---
+++ node-jsmysql-app/src/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const flash = require('connect-flash');
+const MySQLStore = require('express-mysql-session');
+const session = require('express-session');
+
+const {database } = require('./keys');
+
+//initializations 
+
+const app = express();
+
+
+//settings 
+app.set('port', process.env.PORT || 4000);
+//decirle a node donde esta la carpeta views
+
+app.set('views', path.join(__dirname, 'views'));
+
+//configurar handlerbars
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+
+//middlewares
+app.use(session({
+    secret: 'mysqlnotejairist',
+    resave:false,
+    saveUninitialized:false,
+    sotre: new MySQLStore(database)
+}));
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//Global Variables
+app.use((req, res, next)=> {
+    app.locals.success = req.flash('success');
+    next();
+});
+
+//preparar el middleware
+app.use((req, res,next) => {
+    next();
+} );
+
+//Routes
+app.use(require('./routes/'));
+app.use(require('./routes/authentication'));
+app.use('/links',require('./routes/links'));
+
+//public 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Starting the server
+
+app.listen(app.get('port'), ()=> {
+    console.log('Server on port', app.get('port'));
+});
